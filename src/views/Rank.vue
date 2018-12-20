79c58d0c45feeb7bd06603f255dc1b53343f57ed
lihaozecq <lihaozecq@gmail.com>
2018-12-20 23:11:31 +0800

    rank page

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -1,5 +1,59 @@
 <template>
   <div class="rank">
-    <h1>rank</h1>
+    <ScrollView v-show="movieList.length" :data="movieList">
+      <Card
+        v-for="(movie, index) in movieList"
+        :key="movie._id"
+        :movie="movie"
+        :sort="index + 1"
+        @select="gotoDetail"
+      />
+    </ScrollView>
+    <div v-show="!movieList.length" class="loading-wrap">
+      <Loading/>
+    </div>
   </div>
 </template>
+
+<script>
+import ScrollView from 'components/ScrollView'
+import Loading from 'components/Loading'
+import Card from 'components/Card'
+
+export default {
+  components: {
+    ScrollView,
+    Loading,
+    Card
+  },
+  data () {
+    return {
+      movieList: []
+    }
+  },
+  created () {
+    this.getMovieList()
+  },
+  methods: {
+    getMovieList () {
+      this.$axios.get('/api/movie/get_rank').then(res => {
+        if (res.code === 1001) {
+          this.movieList = this.movieList.concat(res.result.movies)
+        }
+      })
+    },
+    gotoDetail (id) {
+      this.$router.push(`/movie/${id}`)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.rank
+  height 100%
+  .loading-wrap
+    display flex
+    align-items center
+    height 100%
+</style>
